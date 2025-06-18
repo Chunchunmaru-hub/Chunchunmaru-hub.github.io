325394a92674552b6ccca1263b45050844aa1b89
Ch1ngz <90413853+Ch1nChun@users.noreply.github.com>
2025-06-18 17:25:15 +0300

    Web Update  #1.3.2
    ## Web update v1.3.2
    ### Main page
    • Improved navbar opacity + blur.
    • Added another secret

--- assets/js/secret.js
+++ assets/js/secret.js
@@ -2,13 +2,18 @@ document.getElementById('zeroTwoBtn').addEventListener('click', function(e) {
   e.preventDefault();
   const video = document.querySelector('.video-bg');
   const audio = document.querySelector('.video-section audio');
-  // Set new sources here:
   video.querySelector('source').src = '/assets/resources/video/ohayo.mp4';
   audio.querySelector('source').src = '/assets/resources/audio/2phuthon.mp3';
-  // Reload and play updated media:
   video.load();
   audio.load();
   video.play();
   audio.play();
+
+  document.title = 'ZeroTwo Mode | Home';
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = '/assets/resources/icons/zero_two.gif';
+  document.getElementsByTagName('head')[0].appendChild(link);
 });
 
